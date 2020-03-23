104deac0d4405870a8cce0f59ddbd0c2eaed748e
CapFreddy <fred9918@hotmail.com>
2020-03-23 21:31:08 +0800

    Packed calculatation model as dll

---
+++ IntersectProjectDLL/Interface.cpp
@@ -0,0 +1,33 @@
+#include "interface.h"
+#include "../IntersectProject/Intersect.h"
+
+
+Intersect intersect;
+
+
+void readFile(string file)
+{
+	intersect.AddGeometryObjectFromFile(file);
+	return;
+}
+
+
+void addGeometryObject(string geoObject)
+{
+	intersect.AddGeometryObjectByString(geoObject);
+	return;
+}
+
+
+void removeGeometryObject(string geoObject)
+{
+	intersect.RemoveGeometryObjectByString(geoObject);
+	return;
+}
+
+
+pair<vector<string>, vector<Point>> getResult()
+{
+	intersect.CalculateIntersections();
+	return pair<vector<string>, vector<Point>>(intersect.GetGeoObjects(), intersect.GetIntersections());
+}
